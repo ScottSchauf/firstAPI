0f9f4364069d5c3cbb7924c1192a871394b87093
ScottSchauf <scottschauf@gmail.com>
2021-02-08 09:59:57 -0500

    second commit

---
+++ firstAPI.js
@@ -0,0 +1,39 @@
+const baseURL = 'https://ghibliapi.herokuapp.com/films';
+const randomizer = document.querySelector('.random');
+
+randomizer.addEventListener('click', fetchMovie);
+
+function fetchMovie(e) {
+
+    let x = getRandomInt();
+    function getRandomInt(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(19);
+        return Math.floor(Math.random() * (19 - 0) + 0);
+      }
+
+    e.preventDefault();
+
+    fetch(baseURL)
+        .then(results => {
+            return results.json()
+        })
+        .then(json => {
+            console.log(json);
+            displayResults(json);
+        });
+
+function displayResults(json) {
+    let nameData = json[x].title;
+    let directorData = json[x].director;
+    let dateData = json[x].release_date;
+    let scoreData = json[x].rt_score;
+    let summaryData = json[x].description;
+
+    title.innerHTML = nameData;
+    director.innerHTML = directorData;
+    release.innerHTML = dateData;
+    rating.innerHTML = scoreData;
+    description.innerHTML = summaryData;
+}
+}
